63d8ccf36f8c0f97f0f4f941510a0f5571830a2d
Iamneo <techadmin@iamneo.ai>
2023-09-20 14:14:31 +0000

    Backup Commit

--- pw3.ps1
+++ pw3.ps1
@@ -3,5 +3,9 @@ $month = Read-Host "Enter Month: "
 $year = Read-Host "Enter year: "
 
 if ($day -match '^\d{1,2}$' -and $month -match '^d{1,2}$' -and $year -match '^\d{4}$') {
-    $dob = Get-Date 
+    $dob = Get-Date -Year $year -Month $month -Day $day
+    $fd = $dob.ToString("dd-MM-yyyy")
+    Write-Host "Birthday: $fd"
+} else {
+    Write-Host "Invalid Input"
 }

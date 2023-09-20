71cd4d099c4a32eef114a171923392bc03a71237
Iamneo <techadmin@iamneo.ai>
2023-09-20 14:19:31 +0000

    Backup Commit

--- pw3.ps1
+++ pw3.ps1
@@ -1,8 +1,8 @@
-$date = Read-Host "Enter date: "
+$day = Read-Host "Enter day: "
 $month = Read-Host "Enter Month: "
 $year = Read-Host "Enter year: "
 
-if ($day -match '^\d{1,2}$' -and $month -match '^d{1,2}$' -and $year -match '^\d{4}$') {
+if ($day -match '^\d{1,2}$' -and $month -match '^\d{1,2}$' -and $year -match '^\d{4}$') {
     $dob = Get-Date -Year $year -Month $month -Day $day
     $fd = $dob.ToString("dd-MM-yyyy")
     Write-Host "Birthday: $fd"

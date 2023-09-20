96c149188886b80306f235b1956bce087018a128
Iamneo <techadmin@iamneo.ai>
2023-09-20 14:34:31 +0000

    Backup Commit

--- pw5.ps1
+++ pw5.ps1
@@ -6,4 +6,16 @@ $subFolder2=Join-Path -Path $mainFolder -ChildPath "SubFolder2"
 New-Item -ItemType Directory -Path $subFolder1 -Force
 New-Item -ItemType Directory -Path $subFolder2 -Force
 
-1..3 | 
+1..3 | ForEach-Object {
+    $filename = "Test$_.txt"
+    $filePath = Join-Path -Path $subFolder1 -ChildPath $filename
+    New-Item -ItemType File -Path $filePath -Force
+}
+
+4..6 | ForEach-Object {
+    $filename = "Test$_.txt"
+    $filePath = Join-Path -Path $subFolder2 -ChildPath $filename
+    New-Item -ItemType File -Path $filePath -Force
+}
+
+Write-Host ""

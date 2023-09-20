6c5a3712a7ac2ecb4c3588ec841d7884d921c329
Iamneo <techadmin@iamneo.ai>
2023-09-20 14:47:21 +0000

    Backup Commit

--- pw2.ps1
+++ pw2.ps1
@@ -1,8 +1,7 @@
-New-Item file.txt
-$filePath = "/home/code/project/workspace/file.txt"
+$filePath = "/home/coder/project/workspace/file.txt"
 
 $textContent = @"
-Welecome to powershell scripting
+Welcome to powershell scripting
 "@
 
 $textContent | Set-Content -Path $filePath

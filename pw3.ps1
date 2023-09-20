817e47e6f8b0bcaf8b4fbbcbc5fdc34b58c947b9
Iamneo <techadmin@iamneo.ai>
2023-09-20 07:25:40 +0000

    Backup Commit

--- pw3.ps1
+++ pw3.ps1
@@ -1,5 +1,5 @@
 $date=Read-Host "Enter date:"
-$month=Read-Host "Enter Month"
-$year=Read-Host "Enter year"
+$month=Read-Host "Enter Month:"
+$year=Read-Host "Enter year:"
 $DOB = "{0:D2}-{1:D2}-{2-D4}" -f $date, $month, $year
-Write-Host "Date-of-Birth: $DOB"
+Write-Host "Date of Birth: $DOB"
